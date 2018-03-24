4c09eefa9a9cde1a1ea5bb9e3cacc6145e45252c
Michal Stawinski <michal.stawinski@gmail.com>
2018-03-24 04:48:02 +0100

    Change partition layout to work from a single mmc0 card
    This change allows uboot to works from mmc0 together with Android. In order to
    get a complete set, uboot update is required - old version of uboot is saving
    env on mmc1 (which is not required anymore).

--- mksdcard.sh
+++ mksdcard.sh
@@ -87,21 +87,41 @@ umount ${diskname}${prefix}*
 sudo parted -a minimal \
 -s ${diskname} \
 unit MiB \
-mklabel gpt \
-mkpart boot 20 40 \
-mkpart recovery 40 60 \
-mkpart extended 60 62 \
-mkpart data 1620 100% \
-mkpart system 62 1086 \
-mkpart cache 1086 1598 \
-mkpart vendor 1598 1608 \
-mkpart misc 1608 1618 \
-mkpart crypt 1618 1620 \
+mklabel msdos \
+mkpart primary ext4 20 41 \
+mkpart primary ext4 41 62 \
+mkpart extended 62 100% \
+mkpart logical ext4 1625 100% \
+mkpart logical ext4 63 1087 \
+mkpart logical ext4 1088 1600 \
+mkpart logical ext4 1601 1611 \
+mkpart logical ext4 1612 1622 \
+mkpart logical ext4 1623 1625 \
 print
+
+# 1      20,0MiB  40,0MiB  20,0MiB  primary   ext4         lba
+# 2      41,0MiB  61,0MiB  20,0MiB  primary   ext4         lba
+# 3      62,0MiB  3724MiB  3662MiB  extended               lba
+# 6      63,0MiB  1087MiB  1024MiB  logical   ext4         lba
+# 7      1088MiB  1600MiB  512MiB   logical   ext4         lba
+# 8      1601MiB  1611MiB  10,0MiB  logical   ext4         lba
+# 9      1612MiB  1622MiB  10,0MiB  logical   ext4         lba
+#10      1623MiB  1625MiB  2,00MiB  logical   ext4         lba
+# 5      1625MiB  3724MiB  2099MiB  logical   ext4         lba
+
+PART_boot=1
+PART_recovery=2
+PART_extended=3
+PART_data=5
+PART_system=6
+PART_cache=7
+PART_vendor=8
+PART_misc=9
+PART_crypt=10
 
 sudo partprobe && sleep 1
 
-for n in `seq 1 8` ; do
+for n in ${PART_boot} ${PART_recovery} ${PART_data} ${PART_system} ${PART_cache} ${PART_vendor} ${PART_misc} ${PART_crypt} ; do
    if ! [ -e ${diskname}${prefix}$n ] ; then
       echo "--------------missing ${diskname}${prefix}$n" ;
       exit 1;
@@ -112,28 +132,39 @@ done
 echo "all partitions present and accounted for!";
 
 echo "------------------making boot partition"
-mkfs.ext4 -F -L boot ${diskname}${prefix}1
+mkfs.ext4 -F -L boot ${diskname}${prefix}${PART_boot}
 sync && sleep 1
 echo "------------------making recovery partition"
-mkfs.ext4 -F -L recovery ${diskname}${prefix}2
+mkfs.ext4 -F -L recovery ${diskname}${prefix}${PART_recovery}
 sync && sleep 1
 echo "------------------making data partition"
-mkfs.ext4 -F -L data ${diskname}${prefix}4
+mkfs.ext4 -F -L data ${diskname}${prefix}${PART_data}
+sync && sleep 1
+echo "------------------making system partition"
+mkfs.ext4 -F -L system ${diskname}${prefix}${PART_system}
 sync && sleep 1
 echo "------------------making cache partition"
-mkfs.ext4 -F -L cache ${diskname}${prefix}6
+mkfs.ext4 -F -L cache ${diskname}${prefix}${PART_cache}
 sync && sleep 1
 echo "------------------making vendor partition"
-mkfs.ext4 -F -L vendor ${diskname}${prefix}7
+mkfs.ext4 -F -L vendor ${diskname}${prefix}${PART_vendor}
+sync && sleep 1
+echo "------------------making misc partition"
+mkfs.ext4 -F -L misc ${diskname}${prefix}${PART_misc}
+sync && sleep 1
+echo "------------------making crypt partition"
+mkfs.ext4 -F -L crypt ${diskname}${prefix}${PART_crypt}
 sync && sleep 1
 
-echo "------------------mounting boot, recovery, data partitions"
+
+echo "------------------mounting boot, recovery, data, vendor partitions"
 sync && sudo partprobe && sleep 5
 
-for n in 1 2 4 7 ; do
+for n in ${PART_boot} ${PART_recovery} ${PART_data} ${PART_vendor} ; do
    echo "--- mounting ${diskname}${prefix}${n}";
    ${mount} ${diskname}${prefix}${n}
 done
+
 
 sudo cp -rfv out/target/product/$product/boot/* ${mountpoint}/boot/
 sudo cp -rfv out/target/product/$product/boot/* ${mountpoint}/recovery/
@@ -146,20 +177,20 @@ if [ -e ${diskname}${prefix}5 ]; then
    system_img=out/target/product/$product/system.img
    file $system_img | grep sparse > /dev/null
    if [ $? -eq 0 ] ; then
-      sudo ./out/host/linux-x86/bin/simg2img $system_img ${diskname}${prefix}5
+      sudo ./out/host/linux-x86/bin/simg2img $system_img ${diskname}${prefix}${PART_system}
    else
-      sudo dd if=$system_img of=${diskname}${prefix}5 bs=1M
+      sudo dd if=$system_img of=${diskname}${prefix}${PART_system} bs=1M
    fi
    sync && sleep 1
-   sudo e2fsck -f ${diskname}${prefix}5
+   sudo e2fsck -f ${diskname}${prefix}${PART_system}
 else
-   echo "-----------missing ${diskname}${prefix}5";
+   echo "-----------missing ${diskname}${prefix}${PART_system}";
 fi
 
 ubootimage=bootable/bootloader/uboot-imx/u-boot.imx
 if [ -e ${ubootimage} ]; then
-   echo "FIXME copying u-boot to i 0x400 is not possible, because partitions description is there..."
-   #sudo dd if=${ubootimage} of=${diskname} bs=512 seek=2 conv=fsync
+#   echo "FIXME copying u-boot to i 0x400 is not possible, because partitions description is there..."
+   sudo dd if=${ubootimage} of=${diskname} bs=512 seek=2 conv=fsync
 else
    echo "-----------missing ${ubootimage} - you should build it, and then try again (or load uboot manually)"
 fi
