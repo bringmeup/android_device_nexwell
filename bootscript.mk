086f5d3d79333561d93fd86c4460eb5ab742e159
Michal Stawinski <michal.stawinski@gmail.com>
2018-04-02 17:30:42 +0200

    Simplify 6x_bootscript and add upgrade.txt script
    upgrade.txt script can be invoked by:
    - stop boot process
    - invoke 'run upgradeu'
    
    This will scan for u-boot.nexo on /boot partition and flash it if it's
    different than the one that is currently flashed. After burning it, board
    will be reset.

--- bootscript.mk
+++ bootscript.mk
@@ -4,17 +4,12 @@ $(BOOTSCRIPT_TARGET): device/nexwell/$(TARGET_BOOTLOADER_DIR)/6x_bootscript.txt
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
 
 UPGRADE_TARGET := $(PRODUCT_OUT)/boot/upgrade.scr
-$(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/bootscripts/upgrade.txt
+$(UPGRADE_TARGET): device/nexwell/$(TARGET_BOOTLOADER_DIR)/upgrade.txt
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "upgrade script" -d $< $@
 
-OLD_UPGRADE_TARGET := $(PRODUCT_OUT)/boot/6x_upgrade
-$(OLD_UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/nitrogen6x/6x_upgrade.txt
-	mkdir -p $(dir $@)
-	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot loader upgrade script" -d $< $@
-
 .PHONY: bootscript
-bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET) $(OLD_UPGRADE_TARGET)
+bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET)
 
 droidcore: bootscript
 bootimage: bootscript
